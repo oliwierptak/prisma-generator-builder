0963c27a7f7e13b57cc2a89ad366782877a0e43a
Oliwier Ptak <oliwierptak@gmail.com>
2023-07-05 16:52:41 +0200

    added cli command to generate default configuration

---
+++ src/cli/wizard.ts
@@ -0,0 +1,80 @@
+import { confirm, input } from "@inquirer/prompts";
+import { PrismaGeneratorBuilderConfig } from "../lib/types";
+import PluginBin from "../component/generator-plugin/plugin/plugin.bin";
+import PluginGenerator from "../component/generator-plugin/plugin/plugin.generator";
+import PluginPackageJson from "../component/generator-plugin/plugin/plugin.package-json";
+import PluginPrismaSchema from "../component/generator-plugin/plugin/plugin.prisma-schema";
+import PluginReadme from "../component/generator-plugin/plugin/plugin.readme";
+import { PluginCopyFiles } from "../component/generator-plugin/plugin/plugin.copy-files";
+import PrismaGeneratorBuilder from "../prisma-generator-builder";
+
+async function wizard() {
+  const result = {} as PrismaGeneratorBuilderConfig;
+
+  result.provider = await input({
+    message: "Generator provider:",
+    default: "prisma-generator-example",
+  });
+
+  result.prettyName = await input({
+    message: "Generator pretty name:",
+    default: "Prisma Generator Example",
+  });
+
+  result.defaultOutput = await input({
+    message: "Generator output directory:",
+    default: "./prisma-generator-example/",
+  });
+
+  result.name = await input({
+    message: "Generator package name:",
+    default: "prisma-generator-example",
+  });
+
+  result.author = await input({
+    message: "Generator package author:",
+    default: "John Doe",
+  });
+
+  result.version = await input({
+    message: "Generator package version:",
+    default: "1.0.0",
+  });
+
+  result.description = await input({
+    message: "Generator package description:",
+    default: "Prisma ORM Generator Example",
+  });
+
+  result.license = await input({
+    message: "Generator package license:",
+    default: "MIT",
+  });
+
+  result.outputDirectoryRoot = await input({
+    message: "Generator output directory:",
+    default: "../prisma-generator-example/",
+  });
+
+  result.plugins = [
+    new PluginBin(),
+    new PluginGenerator(),
+    new PluginPackageJson(),
+    new PluginPrismaSchema(),
+    new PluginReadme(),
+    new PluginCopyFiles(),
+  ];
+
+  return result;
+}
+
+wizard().then(async (config) => {
+  console.log(config);
+  const answer = await confirm({ message: "Continue?" });
+
+  console.log(answer);
+
+  if (answer) {
+    PrismaGeneratorBuilder.build(config);
+  }
+});
